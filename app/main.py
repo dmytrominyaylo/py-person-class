eaeab4ee25f3dc4d5ddd8995016605b5d6edaca7
Dmytro Minyaylo <dimon9660@gmail.com>
2025-01-24 18:34:50 +0200

    changed py-person-class

--- app/main.py
+++ app/main.py
@@ -15,11 +15,11 @@ def create_person_list(people: list) -> list:
     for person_dict in people:
         person = Person.people[person_dict["name"]]
         if "wife" in person_dict and person_dict["wife"]:
-            partner = Person.people.get(person_dict["wife"])
-            if partner:
-                person.wife = partner
+            spouse = Person.people.get(person_dict["wife"])
+            if spouse:
+                person.wife = spouse
         if "husband" in person_dict and person_dict["husband"]:
-            partner = Person.people.get(person_dict["husband"])
-            if partner:
-                person.husband = partner
+            spouse = Person.people.get(person_dict["husband"])
+            if spouse:
+                person.husband = spouse
     return people_list
